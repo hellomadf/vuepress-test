9bd9602152cf51e324cda3af37a68e95e9825590
hellomadf <2405503541@qq.com>
2024-03-09 16:16:23 +0800

    (fix):base err

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/v-hope/",
+  base: "/vuepress-test/",
 
   lang: "zh-CN",
   title: "文档演示",
